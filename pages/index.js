8938cca4d68cb31c4ed474d30f4fe0afdb6f869b
Abílio Inácio <abilio.torres.03@gmail.com>
2025-04-21 21:53:26 +0000

    fraze de impactante

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function home() {
-    return <h1>Teste</h1>
+    return <h1>Proposta:<br></br>
+        1* Simbora compara um bolinho pra o lanche/n<br></br>
+        2* Mais tarde fazer uma faldinha mal passada<br></br>
+        <br></br>
+        Caso não aprove, faça 10 burpes.<br></br>
+        em caso de aprovação da um grito.<br></br>
+    </h1>
 }
 
 export default home;
