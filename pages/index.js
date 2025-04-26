671f7485baa6eb279f695fa49779b5453a819ab6
Abílio Inácio <abilio.torres.03@gmail.com>
2025-04-26 22:34:14 +0000

    prettier configurado

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 function home() {
-    return <h1>Proposta:<br></br>
-        1* Simbora compara um bolinho pra o lanche/n<br></br>
-        2* Mais tarde fazer uma faldinha mal passada<br></br>
-        <br></br>
-        Caso não aprove, faça 10 burpes.<br></br>
-        em caso de aprovação da um grito.<br></br>
+  return (
+    <h1>
+      Proposta:<br></br>
+      1* Simbora compara um bolinho pra o lanche/n<br></br>
+      2* Mais tarde fazer uma faldinha mal passada<br></br>
+      <br></br>
+      Caso não aprove, faça 10 burpes.<br></br>
+      em caso de aprovação da um grito.<br></br>
     </h1>
+  );
+}
+
+function teste() {
+  console.log("identação errada");
 }
 
 export default home;
