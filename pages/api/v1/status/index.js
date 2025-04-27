d977f1120d9a90ebd89ffe7efa9115a983b71b6d
Abílio Inácio <abilio.torres.03@gmail.com>
2025-04-27 07:00:17 +0000

    troca de nome do banco para `local_db` e implementação de quaintada max de conexoes e conexoes abertas e seus respectivos testes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sun;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "São acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const maxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnections.rows[0].max_connections;
+
+  const openedConnections = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const openedConnectionsValue = openedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: openedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
