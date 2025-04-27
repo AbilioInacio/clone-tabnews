0ea1eea1e89016c060d1ab21c2b35bc90cce7533
Abílio Inácio <abilio.torres.03@gmail.com>
2025-04-27 22:10:29 +0000

    adicionado uma variavel de ambiente na pesquisa de connecções abertas

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -10,9 +10,11 @@ async function status(request, response) {
   const maxConnections = await database.query("SHOW max_connections;");
   const maxConnectionsValue = maxConnections.rows[0].max_connections;
 
-  const openedConnections = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
   const openedConnectionsValue = openedConnections.rows[0].count;
 
   response.status(200).json({
